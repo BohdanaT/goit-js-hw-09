5ef81a48a8b69b0ac31dc69a609685f0a80b5ef2
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-17 00:01:52 +0300

    hw-9-task-1-comm-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
 
+const btnStartRef = document.querySelector('button[data-start]');
+const btnStopRef = document.querySelector('button[data-stop]');
+
+btnStartRef.addEventListener('click', onBtnStartClick);
+btnStopRef.addEventListener('click', onBtnStopClick);
+
+btnStopRef.disabled = true;
+let timerId = null;
+
+function onBtnStartClick() {
+    btnStartRef.disabled = true;
+    btnStopRef.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function onBtnStopClick() {
+    btnStartRef.disabled = false;
+    clearInterval(timerId);
+};
