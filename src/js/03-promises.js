8b34ecc6e0932f6197a67e1f3101424e526d003f
BohdanaT <126656948+BohdanaT@users.noreply.github.com>
2023-08-22 00:04:20 +0300

    hw-9-task-3-comm-1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+  });
+};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.target.elements;
+  let inputDelay = Number(delay.value);
+  let inputStep = Number(step.value);
+  let inputAmount = Number(amount.value);
+
+
+
+  createPromise(2, 1500)
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+};
